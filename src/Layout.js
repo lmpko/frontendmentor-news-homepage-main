99580391b4f4154b0af47bc21f179f0142d012be
lmpko <lmpkoo@gmail.com>
2023-05-17 11:14:38 +0200

    some css improvement, and nav button and nav menu conditional rendering elements on depends screen size added

--- src/Layout.js
+++ src/Layout.js
@@ -3,14 +3,17 @@ import NavbarComponent from './NavbarComponent'
 import ArticleComponent from './ArticleComponent'
 import NewsComponent from './NewsComponent'
 import QuickNewsComponent from './QuickNewsComponent'
+import SideMenuComponent from "./SideMenuComponent"
 
 const Layout = () => {
   return (
     <>
+        <SideMenuComponent />
         <NavbarComponent/>
         <ArticleComponent/>
         <NewsComponent/>
         <QuickNewsComponent/>
+        
     </>
     )
 }
--- src/NavbarComponent.js
+++ src/NavbarComponent.js
@@ -1,18 +1,36 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import logo from "/assets/images/logo.svg"
 import sideMenuImg from "/assets/images/icon-menu.svg"
 import MenuComponent from './MenuComponent'
-import SideMenuComponent from "./SideMenuComponent"
 
 const NavbarComponent = () => {
+  const [showElements, setShowElements] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setShowElements(window.innerWidth <= 768);
+    };
+
+    // Add event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Check initial screen width
+    handleResize();
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <nav>
         <img src={logo} alt="logo" />
-        <MenuComponent/>
-        <SideMenuComponent />
-        <button>
+        {!showElements && <MenuComponent/>}
+        
+        {showElements && <button>
           <img src={sideMenuImg} alt="side-menu-icon"/>
-        </button>
+        </button>}
     </nav>
   )
 }
