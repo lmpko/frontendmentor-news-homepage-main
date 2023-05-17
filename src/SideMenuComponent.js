ecb16fcb22ef930111a2276204b017d75943a4be
lmpko <lmpkoo@gmail.com>
2023-05-17 08:55:21 +0200

    sidemenu component created, body element flex added

---
+++ src/SideMenuComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SideMenuComponent = () => {
+  return (
+    <div>Home
+    New
+    Popular
+    Trending
+    Categories</div>
+  )
+}
+
+export default SideMenuComponent
